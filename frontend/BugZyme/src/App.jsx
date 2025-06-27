6504182a47e75c98ffdade4ed473d4879245dd8d
Awadhesh Prasad <awadhesh2090@gmail.com>
2025-06-27 20:38:41 +0530

    Added pages

--- frontend/BugZyme/src/App.jsx
+++ frontend/BugZyme/src/App.jsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import About from "./pages/About";
+import Home from "./pages/Home";
+import Career from "./pages/Career";
+import ContactUs from "./pages/ContactUs";
+import Blogs from "./pages/Blogs";
 
 const routes = [
-  // { path: "/", element: <App /> },
+  { path: "/", element: <Home /> },
   { path: "/about", element: <About /> },
+  { path: "/home", element: <Home /> },
+  { path: "/career", element: <Career /> },
+  { path: "/contact-us", element: <ContactUs /> },
+  { path: "/blogs", element: <Blogs /> },
 ];
 
 const router = createBrowserRouter(routes);
