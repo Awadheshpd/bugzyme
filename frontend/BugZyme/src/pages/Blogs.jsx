799c9e54a1da3dfdae7bb65fdafbed145d238241
Awadhesh Prasad <awadhesh2090@gmail.com>
2025-06-28 22:26:24 +0530

    Added contact us page

--- frontend/BugZyme/src/pages/Blogs.jsx
+++ frontend/BugZyme/src/pages/Blogs.jsx
@@ -1,3 +1,24 @@
 export default function Blogs() {
-  return <h1>Blogs</h1>;
+  return (
+    <>
+      <div className="blogs-page">
+        <h1>Blog & Training Page</h1>
+        <h2>The BugZyme Blog</h2>
+        <h3>Insights from our QA experts on:</h3>
+        <ul>
+          <li>Regulatory trends in pharma QA</li>
+          <li>AI and automation in testing</li>
+          <li>Real-world audit readiness stories</li>
+        </ul>
+        <h3>Training & Webinars</h3>
+        <ul>
+          <li>Live CSV Masterclasses</li>
+          <li>Agile Testing Bootcamps</li>
+          <li>Free QA Checklists & Templates</li>
+          <li>Pharma + Software QA Certification Prep</li>
+        </ul>
+        <p>[ Subscribe to Newsletter ] [ View Upcoming Trainings ]</p>
+      </div>
+    </>
+  );
 }
--- frontend/BugZyme/src/pages/ContactUs.jsx
+++ frontend/BugZyme/src/pages/ContactUs.jsx
@@ -1,3 +1,21 @@
 export default function ContactUs() {
-    return <h1>Contact Us</h1>;
+  return (
+    <>
+      <div className="contact-us-container">
+        <h1>Contact Us</h1>
+        <h4>Let’s Talk Quality.</h4>
+        <p>
+          We’re ready to understand your QA challenges and offer audit-ready,
+          future-proof solutions.
+        </p>
+        <p>
+          <a>hello@bugzyme.com</a>
+        </p>
+        <p>
+          <a href="www.bugzyme.com">www.bugzyme.com</a>
+        </p>
+        <p>Based in India | Serving Clients Globally</p>
+      </div>
+    </>
+  );
 }
