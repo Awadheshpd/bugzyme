fe848f38cc91f5308f1e0de13ded1b23f2243302
Awadhesh Prasad <awadhesh2090@gmail.com>
2025-06-28 10:10:53 +0530

    Added routing

--- frontend/BugZyme/src/App.jsx
+++ frontend/BugZyme/src/App.jsx
@@ -1,13 +1,22 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Nav } from "react-bootstrap";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
 import About from "./pages/About";
-import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
 import Career from "./pages/Career";
 import ContactUs from "./pages/ContactUs";
-import Blogs from "./pages/Blogs";
+import Home from "./pages/Home";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const routes = [
-  { path: "/", element: <Home /> },
+  { path: "/", element: <Home />, errorElement: <h2>No page found</h2> },
   { path: "/about", element: <About /> },
   { path: "/home", element: <Home /> },
   { path: "/career", element: <Career /> },
@@ -20,6 +29,37 @@ const router = createBrowserRouter(routes);
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Nav variant="tabs" defaultActiveKey="#first">
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/contact-us">Contact Us</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/career">Career</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/blogs">Blogs</Link>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </BrowserRouter>
+
+      <Outlet />
       <RouterProvider router={router} />
     </>
   );
